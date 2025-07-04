ba7f41539381bf00192208fb68d445ec6cbde481
Yuan Zhu <yuan_zhu123@hotmail.com>
2025-07-04 14:47:44 -0400

    update channel compression

--- EigenVecDataset.py
+++ EigenVecDataset.py
@@ -28,12 +28,15 @@ class EigenVecDataset(Dataset):
         self.ev_idx = ev_idx
         self.config_file, config_data = readConfig(simulator_dir, config_filename)
         self.numUEs = config_data['layout']['numOfUEs']
+        self.stepSize = int(config_data['simulationTime']['stepSize'] * 1000)
+        self.numSteps = int(config_data['simulationTime']['simulationLen'] / config_data['simulationTime']['stepSize'])
         self.antPerPanel = config_data['bsAntennaParams']['m'] * config_data['bsAntennaParams']['n'] * config_data['bsAntennaParams']['p']
         self.ueAntPerPanel = config_data['utAntennaParams']['m']* config_data['utAntennaParams']['n'] * config_data['utAntennaParams']['p']
         self.maxRank = min(self.antPerPanel, self.ueAntPerPanel)
+        self.ueSpeed = config_data['ueSpeed']
         self.transform = transform
         config_filesplit = os.path.splitext(config_filename)
-        cache_name = "{}_{}.pt".format(config_filesplit[0], repetition)
+        cache_name = "{}_{}kmH_{}UEs_{}s_{}ms_{}tx_{}rx.pt".format(config_filesplit[0], self.ueSpeed, repetition*self.numUEs, self.numSteps, self.stepSize, self.antPerPanel, self.ueAntPerPanel)
         if exists(cache_name):
             self.eigen_buffer = torch.load(cache_name)
             return
@@ -43,12 +46,12 @@ class EigenVecDataset(Dataset):
         lib.getNumOfPrecodingSBs.restype = c_int
         numOfPrecodingSBs = lib.getNumOfPrecodingSBs(b)
         lib.chanexp.restype = c_char_p
-        EigenFloats = c_float * (2 * self.antPerPanel) * numOfPrecodingSBs * self.maxRank * self.numUEs
+        EigenFloats = c_float * (2 * self.antPerPanel) * (numOfPrecodingSBs*self.numSteps) * self.maxRank * self.numUEs
         EigenBuffer = EigenFloats()
-        self.eigen_buffer = np.zeros((self.numUEs*repetition, self.maxRank, numOfPrecodingSBs, self.antPerPanel*2), dtype=np.float32)
+        self.eigen_buffer = np.zeros((self.numUEs*repetition, self.maxRank, numOfPrecodingSBs*self.numSteps, 2 * self.antPerPanel), dtype=np.float32)
         for i in range(repetition):
             lib.chanexp(b, EigenBuffer)
-            eigen_buffer = np.ctypeslib.as_array(EigenBuffer, (self.numUEs, self.maxRank, numOfPrecodingSBs, self.antPerPanel*2))
+            eigen_buffer = np.ctypeslib.as_array(EigenBuffer, (self.numUEs, self.maxRank, numOfPrecodingSBs*self.numSteps, 2 * self.antPerPanel))
             self.eigen_buffer[i*self.numUEs:(i+1)*self.numUEs,:] = eigen_buffer
         self.eigen_buffer = torch.from_numpy(self.eigen_buffer)
         if not exists(cache_name):
@@ -61,7 +64,7 @@ class EigenVecDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        ev = self.eigen_buffer[idx, self.ev_idx, :]
+        ev = self.eigen_buffer[idx, self.ev_idx, :, :]
         sample = {'ev': ev}
 
         if self.transform:
