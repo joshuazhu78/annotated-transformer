d3968bebda55b4d92d5ea893eeb05897ab10e050
Yuan Zhu <yuan_zhu123@hotmail.com>
2025-07-27 20:24:32 -0400

    add frequency domain channel

--- ChannelTDataset.py
+++ ChannelTDataset.py
@@ -15,11 +15,13 @@ class go_string(Structure):
 class ChannelTDataset(Dataset):
     """Eigen Vector dataset."""
 
-    def __init__(self, config_filename, simulator_dir, numUEs, transform=None):
+    def __init__(self, config_filename, simulator_dir, timeDomainChannel, numUEs, transform=None):
         """
         Arguments:
             config_filename (string): Name of the json config file for xg-simulator.
             simulator_dir (string): Directory of xg-simulator.
+            timeDomainChannel (bool): whether the channel is for time domain or freq domain
+            numUEs (int): number of UEs to generate for data set, set to -1 to use SLS configuration
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
@@ -34,7 +36,7 @@ class ChannelTDataset(Dataset):
         numUEsPerDrop = config_data["layout"]['numOfUEs']
         numOfDrops = int(self.numUEs/numUEsPerDrop)
         config_filesplit = os.path.splitext(config_filename)
-        cache_name = "{}_{}kmH_{}ue_{}s_{}ms_{}rx_{}tx.pt".format(config_filesplit[0], self.ueSpeed, self.numUEs, self.numSteps, self.stepSize, self.ueAntPerPanel, self.bsAntPerPanel)
+        cache_name = "{}_{}_{}kmH_{}ue_{}s_{}ms_{}rx_{}tx.pt".format(config_filesplit[0], timeDomainChannel, self.ueSpeed, self.numUEs, self.numSteps, self.stepSize, self.ueAntPerPanel, self.bsAntPerPanel)
         if exists(cache_name):
             self.h_buffer, self.t_buffer = torch.load(cache_name)
             return
@@ -42,18 +44,21 @@ class ChannelTDataset(Dataset):
         f = self.config_file.encode('utf-8')
         b = go_string(c_char_p(f), len(f))
         lib.getComChanLength.restype = c_int
-        chanLen = lib.getComChanLength(b)
+        if timeDomainChannel:
+            self.chanLen = lib.getComChanLength(b)
+        else:
+            self.chanLen = lib.getNumOfRBs(b)
         lib.chanfexp.restype = c_char_p
-        self.h_buffer = np.zeros((self.numUEs, self.numSteps, self.ueAntPerPanel, self.bsAntPerPanel, chanLen * 2), dtype=np.float32)
-        self.t_buffer = np.zeros((self.numUEs, chanLen), dtype=np.float32)
+        self.h_buffer = np.zeros((self.numUEs, self.numSteps, self.ueAntPerPanel, self.bsAntPerPanel, self.chanLen * 2), dtype=np.float32)
+        self.t_buffer = np.zeros((self.numUEs, self.chanLen), dtype=np.float32)
         for i in range(numOfDrops):
-            ChanFFloats = c_float * (chanLen * 2) * self.bsAntPerPanel * self.ueAntPerPanel *  self.numSteps * numUEsPerDrop
+            ChanFFloats = c_float * (self.chanLen * 2) * self.bsAntPerPanel * self.ueAntPerPanel *  self.numSteps * numUEsPerDrop
             ChanFBuffer = ChanFFloats()
-            ChanLFloats = c_float * chanLen * numUEsPerDrop
+            ChanLFloats = c_float * self.chanLen * numUEsPerDrop
             ChanLBuffer = ChanLFloats()
-            lib.chanfexp(b, -1, ChanFBuffer, ChanLBuffer)
-            h_buffer = np.ctypeslib.as_array(ChanFBuffer, (numUEsPerDrop, self.numSteps, self.ueAntPerPanel, self.bsAntPerPanel, chanLen * 2))
-            t_buffer = np.ctypeslib.as_array(ChanLBuffer, (numUEsPerDrop, chanLen))
+            lib.chanfexp(b, timeDomainChannel, -1, ChanFBuffer, ChanLBuffer)
+            h_buffer = np.ctypeslib.as_array(ChanFBuffer, (numUEsPerDrop, self.numSteps, self.ueAntPerPanel, self.bsAntPerPanel, self.chanLen * 2))
+            t_buffer = np.ctypeslib.as_array(ChanLBuffer, (numUEsPerDrop, self.chanLen))
             self.h_buffer[i*numUEsPerDrop:(i+1)*numUEsPerDrop,:] = h_buffer
             self.t_buffer[i*numUEsPerDrop:(i+1)*numUEsPerDrop,:] = t_buffer
         self.h_buffer = torch.from_numpy(self.h_buffer)
